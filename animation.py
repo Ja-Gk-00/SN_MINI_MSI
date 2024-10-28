ac1500d16111a55ae5917b90ee8a6abeba098638
Filip Możdżonek <01159051@pw.edu.pl>
2024-10-28 19:39:02 +0100

    Add weigths diff showcase, Animation for classification

--- animation.py
+++ animation.py
@@ -54,9 +54,14 @@ class NodeOutEdges(VGroup):
         ]
         super().__init__(*self.edges)
 
-    def set_weights(self, weights) -> None:
+    def set_weights(self, weights, backpropagate = False) -> None:
         for w, edge in zip(weights, self.edges):
-            edge.set_opacity(w)
+            if backpropagate:
+                edge.set_color(YELLOW)
+                edge.set_opacity(abs(w))
+            else:
+                edge.set_color(RED if w > 0 else BLUE)
+                edge.set_opacity(abs(w))
 
 
 class LayerOutEdges(VGroup):
@@ -66,9 +71,9 @@ class LayerOutEdges(VGroup):
         ]
         super().__init__(*self.edges)
 
-    def set_weights(self, weights) -> None:
+    def set_weights(self, weights, backpropagate = False) -> None:
         for w, edges in zip(weights, self.edges):
-            edges.set_weights(w)
+            edges.set_weights(w, backpropagate)
 
 
 class Edges(VGroup):
@@ -86,47 +91,50 @@ class Edges(VGroup):
 def get_weights(network: NN):
     return [l.weights for l in network.layers]
 
-def normalize_values(matrix: np.array):
+def normalize_min_max(matrix: np.array):
     v_min = np.min(matrix)
     v_max = np.max(matrix)
     return (matrix - v_min) / (v_max - v_min)
 
+
 def normalize_weights_layers(weights):
-    return [normalize_values(w) for w in weights]
+    return [Sigmoid()(w) * 2 - 1 for w in weights]
 
 
 class NNAnimation(Scene):
     def construct(self):
+        np.random.seed(42)
         df = pd.read_csv("data/classification/data.three_gauss.train.100.csv")
-        x = np.array(df.iloc[:,:-1]).reshape((300, 2))
+        x = np.array(df.iloc[:,:-1]).reshape((-1, 2))
         y = np.array(df.iloc[:,-1])
         num_classes = 3
 
         one_hot_encoded = np.zeros((y.size, num_classes))
         one_hot_encoded[np.arange(y.size), y - 1] = 1
-        one_hot_encoded = one_hot_encoded.reshape((300, 3))
+        one_hot_encoded = one_hot_encoded.reshape((-1, num_classes))
 
         nn1 = NN(input_shape=(0,2))
-        nn1.add_new_random_layer(4, GELU())
-        nn1.add_new_random_layer(4, GELU())
-        nn1.add_new_random_layer(3, activation=Linear())
+        nn1.add_new_random_layer(3, ELU())
+        nn1.add_new_random_layer(3, ELU())
+        nn1.add_new_random_layer(3, activation=Softmax())
 
-        nodes = Nodes([2, 4, 4, 3])
+        nodes = Nodes([2, 3, 3, 3])
         edges = Edges(nodes)
         self.play(Create(nodes))
         self.play(Create(edges))
         self.wait(DELAY)
         edges.set_weights(normalize_weights_layers(get_weights(nn1)))
 
-        LR = 0.01
+        LR = 0.0003
         ANIMATION_START = 0
-        ANIMATION_END = 10_000
+        ANIMATION_END = 15_000
         ANIMATION_STEP = 1000
-        for step in range(10_000):
+        loss = LogisticCrossEntropy()
+        for step in range(ANIMATION_END):
             index = np.random.randint(x.shape[0])
             x_i = x[index]
-            y_i = y[index]
-            yhat_i = nn1.apply(x_i)
+            y_i = one_hot_encoded[index]
+            yhat_i = nn1.apply(x_i.reshape(1, -1))
 
             # forward pass animation
             if step >= ANIMATION_START and step < ANIMATION_END and (step % ANIMATION_STEP) == 0:
@@ -134,12 +142,13 @@ class NNAnimation(Scene):
                 self.wait(DELAY)
                 vec = x_i
                 for l_idx, layer in enumerate(nodes.layers):
-                    layer.set_values(normalize_values(vec))
+                    layer.set_values(normalize_min_max(vec))
                     if l_idx < len(nodes.layers) - 1:
                         vec = vec @ W[l_idx]
                     self.wait(DELAY)
 
-            g, gb = nn1.backpropagate(yhat_i, y_i)
+            old_W = normalize_weights_layers(get_weights(nn1))
+            g, gb = nn1.backpropagate(yhat_i, y_i, loss)
             for i in range(len(nn1.layers)):
                 nn1.layers[i].weights -= LR * g[i]
                 nn1.layers[i].bias -= LR * gb[i]
@@ -147,8 +156,15 @@ class NNAnimation(Scene):
             # back propagation animation
             if step >= ANIMATION_START and step < ANIMATION_END and (step % ANIMATION_STEP) == 0:
                 W = normalize_weights_layers(get_weights(nn1))
+                # show weights diffs
+                for w_idx in range(len(nodes.layers) - 2, -1, -1):
+                    edges.edges_groups[w_idx].set_weights(
+                        normalize_min_max(abs(W[w_idx] - old_W[w_idx])),
+                        backpropagate=True,
+                        )
+                    self.wait(DELAY)
+                # show weights
                 for w_idx in range(len(nodes.layers) - 2, -1, -1):
                     edges.edges_groups[w_idx].set_weights(W[w_idx])
-                    self.wait(DELAY)
                 nodes.set_zeros()
         self.wait(5)
